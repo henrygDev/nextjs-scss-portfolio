a01ef3364cb72ec65f084536406272d91670915a
Henry <henryyguan@gmail.com>
2021-09-28 00:15:04 -0400

    added resume link

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -7,6 +7,7 @@ const Contact = () => {
     <div className={styles.contact} id="contact">
       <div className={styles.text}>
         <a href="mailto:henryyguan@gmail.com">Email<span></span></a><br/>
+        <a href="https://drive.google.com/file/d/1duC13Mz2B01_FieAFAimhMHd7RjnuAEK/view?usp=sharing">Resume</a><br/>
         <a href="https://www.linkedin.com/in/henry-guan-a1a772142/">LinkedIn</a><br/>
         <a href="https://github.com/henrygeez">GitHub</a>
       </div>
