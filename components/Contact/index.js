fb862fdc13ab5dd61592f6909a7254091f9268f3
Henry <henryyguan@gmail.com>
2021-09-28 05:51:19 -0400

    reverted new tab change

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -7,9 +7,9 @@ const Contact = () => {
     <div className={styles.contact} id="contact">
       <div className={styles.text}>
         <a href="mailto:henryyguan@gmail.com">Email<span></span></a><br/>
-        <a target="_blank" href="https://drive.google.com/file/d/1duC13Mz2B01_FieAFAimhMHd7RjnuAEK/view?usp=sharing">Resume</a><br/>
-        <a target="_blank" href="https://www.linkedin.com/in/henry-guan-a1a772142/">LinkedIn</a><br/>
-        <a target="_blank" href="https://github.com/henrygeez">GitHub</a>
+        <a href="https://drive.google.com/file/d/1duC13Mz2B01_FieAFAimhMHd7RjnuAEK/view?usp=sharing">Resume</a><br/>
+        <a href="https://www.linkedin.com/in/henry-guan-a1a772142/">LinkedIn</a><br/>
+        <a href="https://github.com/henrygeez">GitHub</a>
       </div>
     </div>
   )
