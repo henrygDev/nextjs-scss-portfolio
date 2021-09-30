97237b58bc7c622913da48bcd2ab9e1c6c7685a0
Henry <henryyguan@gmail.com>
2021-09-30 07:55:48 -0400

    added footer note

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -5,11 +5,15 @@ import styles from './Contact.module.scss';
 const Contact = () => {
   return (
     <div className={styles.contact} id="contact">
+      <div className={styles.block}></div>
       <div className={styles.text}>
         <a href="mailto:henryyguan@gmail.com">Email<span></span></a><br/>
         <a href="https://drive.google.com/file/d/1duC13Mz2B01_FieAFAimhMHd7RjnuAEK/view?usp=sharing">Resume</a><br/>
         <a href="https://www.linkedin.com/in/henry-guan-a1a772142/">LinkedIn</a><br/>
         <a href="https://github.com/henrygeez">GitHub</a>
+      </div>
+      <div className={styles.footer}>
+        Designed and developed by Henry Guan.
       </div>
     </div>
   )
